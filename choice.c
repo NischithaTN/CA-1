0443276e6778ad7ec61ad7de42ce1ab0b22f852f
NischithaTN <nischitha.n@s.amity.edu>
2025-03-25 15:42:27 +0530

    case 3

---
+++ choice.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+
+    printf("Choose a branch:\n");
+    printf("1. CSE\n");
+    printf("2. AIML\n");
+    printf("3. ECE\n");
+    printf("4. Robotics\n");
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch (choice) {
+        case 1:
+            printf("You chose CSE.\n");
+            break;
+       case 2:
+            printf("you chose AIML.\n");
+            break;
+       case 3:
+           printf("you chose ECE.\n");
+          break;
